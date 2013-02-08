aafa1a8959deedf460f217962e808a373900d55a
matt <mvr@laptop>
2013-02-08 09:53:48 -0500

    testing

--- django_hudson/management/commands/xmltest.py
+++ django_hudson/management/commands/xmltest.py
@@ -4,18 +4,6 @@ from optparse import make_option
 from django.core.management.base import BaseCommand
 from django_hudson.xmlrunner import XmlDjangoTestSuiteRunner
 
-try:
-    import south
-    try:
-        from south.management.commands import patch_for_test_db_setup
-    except Exception:
-        #Just in case the south paths change, we want to fail fast and loud
-        raise
-except ImportError:
-    #Must not have south
-    def patch_for_test_db_setup():
-        pass
-
 class Command(BaseCommand):
     help = "Runs the test suite with reporting to xml"
 
@@ -28,15 +16,16 @@ class Command(BaseCommand):
 
 
     def handle(self, *test_labels, **options):
+        # delay this import, because south might import the world
+        from south.management.commands import patch_for_test_db_setup
         patch_for_test_db_setup()
         output_dir=options.get('output_dir')
-        
+
         verbosity = int(options.get('verbosity', 1))
         interactive = options.get('interactive', True)
-        
+
         test_runner = XmlDjangoTestSuiteRunner(output_dir=output_dir, interactive=interactive, verbosity=verbosity)
         failures = test_runner.run_tests(test_labels)
 
         if failures:
             sys.exit(bool(failures))
-
